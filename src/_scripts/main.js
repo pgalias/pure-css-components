57fc4595ef4f5522ed1d7942b105a0770f82aa22
pgalias <pawgalias@gmail.com>
2016-09-20 00:13:12 +0200

    slider, dropdown

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -10,7 +10,7 @@ $(() => {
 
   function onChange(self, child, regexp, replace = '', checkbox = false) {
     const txt = self.attr('data-class-name');
-    const selector = self.closest('article').find(child);
+    const selector = self.closest('article').find('pre').find(child);
     const type = child === 'code' ? 'html' : 'text';
     const code = selector[type]();
 
@@ -25,17 +25,20 @@ $(() => {
     selector[type](replace);
   }
 
-  $('.acc-width-opt input[type=radio],.tabs-width-opt input[type=radio],.modalbox-width-opt input[type=radio]').on('change', function() {
+  $('.acc-width-opt input[type=radio],.tabs-width-opt input[type=radio],.modalbox-width-opt input[type=radio],.slider-width-opt input[type=radio]').on('change', function() {
     const self = $(this);
 
-    const regexp = /(--full-width|--fixed-width|--wide|--wide-short|--full-size|--fixed-size)/g;
+    const regexp = /(--full-width|--fixed-width|--wide-short|--wide|--full-size|--fixed-size)/g;
     onChange(self, 'code', regexp);
 
-    self.closest('article').find('.accordion, .tabs').toggleClass((index, element) => {
+    self.closest('article').find('.accordion, .tabs, .slider').toggleClass((index, element) => {
       let elementArray = element.split(' ');
       return `${elementArray[0]}--full-width ${elementArray[0]}--fixed-width`
     });
-    self.closest('article').find('.modal__dialog').removeClass('modal__dialog--full-size modal__dialog--fixed-size modal__dialog--wide modal__dialog--wide-short').addClass(`modal__dialog${self.attr('data-class-name')}`);
+
+    self.closest('article').find('.modal__dialog')
+        .removeClass('modal__dialog--full-size modal__dialog--fixed-size modal__dialog--wide modal__dialog--wide-short')
+        .addClass(`modal__dialog${self.attr('data-class-name')}`);
   });
 
   $('.acc-type-opt input[type=radio]').on('change', function() {
@@ -49,16 +52,26 @@ $(() => {
     self.closest('article').find('.accordion :input').each((index, element) => $(element).attr('type', txt));
   });
 
-  $('.acc-theme-opt input[type=checkbox], .tabs-theme-opt input[type=checkbox]').on('change', function() {
+  $('.slider-pills-position-opt input[type=radio]').on('change', function() {
     const self = $(this);
     const txt = self.attr('data-class-name');
-    const regexp = /(accordion--blue|tabs--blue)/;
+    const regexp = /(bottom|top|left|right)/;
+
+    onChange(self, '.hljs-tag:eq(36)', regexp, '');
+
+    self.closest('article').find('.slider__pills').removeClass('slider__pills--bottom slider__pills--top slider__pills--left slider__pills--right').addClass(`slider__pills--${txt}`);
+  });
+
+  $('.acc-theme-opt input[type=checkbox], .tabs-theme-opt input[type=checkbox], .slider-theme-opt input[type=checkbox]').on('change', function() {
+    const self = $(this);
+    const txt = self.attr('data-class-name');
+    const regexp = /(accordion--blue|tabs--blue|slider--blue)/;
 
     onChange(self, '.hljs-tag:first .hljs-string', regexp, '', true);
 
-    self.closest('article').find('.accordion, .tabs, .modal__dialog').toggleClass((index, element) => {
+    self.closest('article').find('.accordion, .tabs, .slider').toggleClass((index, element) => {
       let elementArray = element.split(' ');
-      return `${elementArray[0]}${txt}`;
+      return `${elementArray[0]}${txt.replace(elementArray[0], '')}`;
     });
   });
 
@@ -69,6 +82,6 @@ $(() => {
 
     onChange(self, '.hljs-tag:eq(5) .hljs-string', regexp, '', true);
 
-    self.closest('article').find('.modal__dialog').toggleClass(`modal__dialog${txt}`);
+    self.closest('article').find('.modal__dialog').toggleClass(txt);
   });
 });
